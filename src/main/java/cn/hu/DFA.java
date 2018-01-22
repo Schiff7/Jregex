02efc1dd10583e0bec91b3aa09ddb7aa06a92629
Hu <apophisy@live.cn>
2018-01-23 00:21:20 +0800

    before test.

--- src/main/java/cn/hu/DFA.java
+++ src/main/java/cn/hu/DFA.java
@@ -1,8 +1,76 @@
 package cn.hu;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
  * DFA
  */
 class DFA {
+    private Map<Pairs, List<State>> map;
+    private List<List<State>> states;
+    private List<State> initState;
+    private StringBuffer operands;
 
+    public DFA() {
+        this.map = new HashMap<>();
+        this.states = new ArrayList<>();
+        this.initState = new ArrayList<>();
+        this.operands = new StringBuffer();
+    }
+
+    public static class Pairs {
+        private List<State> state;
+        private String string;
+        
+        public Pairs(List<State> state, String string) {
+            this.state = state;
+            this.string = string;
+        }
+        /**
+         * @return the state
+         */
+        public List<State> getState() {
+            return state;
+        }
+        /**
+         * @return the string
+         */
+        public String getString() {
+            return string;
+        }
+    }
+
+    /**
+     * @return the map
+     */
+    public Map<Pairs, List<State>> getMap() {
+        return map;
+    }
+    /**
+     * @return the states
+     */
+    public List<List<State>> getStates() {
+        return states;
+    }
+    /**
+     * @return the initState
+     */
+    public List<State> getInitState() {
+        return initState;
+    }
+    /**
+     * @param initState the initState to set
+     */
+    public void setInitState(List<State> initState) {
+        this.initState = initState;
+    }
+    /**
+     * @return the operands
+     */
+    public StringBuffer getOperands() {
+        return operands;
+    }
 }
--- src/main/java/cn/hu/Jregex.java
+++ src/main/java/cn/hu/Jregex.java
@@ -1,6 +1,13 @@
 package cn.hu;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 import java.util.Stack;
+import java.util.stream.Collectors;
 
 /**
  * Jregex
@@ -12,21 +19,20 @@ public class Jregex {
 
     public Jregex(String raw) {
         this.raw = raw;
-        this.NFA = toNFA();
+        this.NFA = toNFA(this.raw);
+        this.DFA = toDFA(this.NFA);
     }
 
     /**
      * @return to NFA
      */
-    public NFA toNFA() {
+    public NFA toNFA(String s) {
         Stack<NFA> operandStack = new Stack<>();
         Stack<MetaChr> operatorStack = new Stack<>();
         int combo = 0;
-        for(int i = 0; i < this.raw.length();) {
-            String p = String.valueOf(this.raw.charAt(i));
+        for(int i = 0; i < s.length();) {
+            String p = String.valueOf(s.charAt(i));
             MetaChr mc = MetaChr.map(p);
-            System.out.println(operatorStack);
-            System.out.println(operandStack);
             if(null != mc) {
                 if(i == 0 && mc.optType() == MetaChr.OperationType.DELIMITER) {
                     operatorStack.push(mc);
@@ -41,7 +47,6 @@ public class Jregex {
                     i++;
                 } else {
                     MetaChr tmp = operatorStack.pop();
-                    System.out.println(tmp);
                     switch(tmp.optType()) {
                         case PREFIX:
                             break;
@@ -105,6 +110,70 @@ public class Jregex {
             }
         }
         return operandStack.pop();
+    }
+
+    public DFA toDFA(NFA n) {
+        class Anonymous {
+            private DFA DFA;
+            Anonymous() {
+                this.DFA = new DFA();
+                Map<NFA.Pairs, State> initTransitions = n.getInitState().getTransitions(); 
+                List<State> initState = initTransitions.keySet()
+                    .stream()
+                    .filter(x -> x.getString() == null)
+                    .map(y -> initTransitions.get(y))
+                    .collect(Collectors.toList());
+                this.DFA.setInitState(initState);
+                epsilonClosure(Arrays.asList(initState));
+            }
+            List<State> move(List<State> l) {
+                Set<State> set = new HashSet<>();
+                List<State> r = new ArrayList<>( l.stream()
+                    .map(x -> {
+                        return x.getTransitions().keySet()
+                            .stream()
+                            .filter(y -> y.getString() == null)
+                            .map(z -> x.getTransitions().get(z))
+                            .collect(Collectors.toSet());
+                    })
+                    .reduce(set, (acc, item) -> {
+                        acc.addAll(item);
+                        return acc;
+                    })
+                );
+                return r;
+            }
+            List<List<State>> epsilonClosure(List<List<State>> states) {
+                List<List<State>> r = new ArrayList<>();
+                states.stream().map(state -> {
+                    for(int i = 0; i < n.getOperands().length(); i++) {
+                        String s = String.valueOf(n.getOperands().charAt(i));
+                        Set<State> set = new HashSet<>();
+                        List<State> l = new ArrayList<>( state.stream()
+                            .map(x -> {
+                                return x.getTransitions().keySet()
+                                    .stream()
+                                    .filter(y -> y.getString().equals(s))
+                                    .map(z -> x.getTransitions().get(z))
+                                    .collect(Collectors.toSet());
+                            })
+                            .reduce(set, (acc, item) -> {
+                                acc.addAll(item);
+                                return acc;
+                            })
+                        );
+                        l = this.move(l);
+                        r.add(l);
+                        this.DFA.getStates().add(l);
+                        this.DFA.getMap().put(new DFA.Pairs(state, s), l);
+                    }
+                    return r;
+                });
+                return epsilonClosure(r);
+            }
+        }
+
+        return new Anonymous().DFA;
     }
     /**
      * @return to DFA
